422942151781380dca48e188b1b0852c3a1de7db
Harper <git@harperandrews.org>
2025-06-29 17:11:24 -0500

    Add WIP yaak template

---
+++ user/yaak/template.py
@@ -0,0 +1,12 @@
+pkgname = "yaak"
+pkgdesc = "Desktop API client"
+pkgver = "2025.4.0"
+pkgrel = 0
+url = "https://yaak.app"
+license = "MIT"
+
+depends = [
+    "libwebkitgtk",
+]
+
+# TODO(Harper): Finish

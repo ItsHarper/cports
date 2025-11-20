28a8571beb4e884ebbae9ce23ed05595114287ec
ItsHarper <git-f4cbe@harperandrews.org>
2025-11-20 16:36:15 -0600

    user/topiary: new package

---
+++ user/topiary/template.py
@@ -0,0 +1,33 @@
+pkgname = "topiary"
+pkgver = "0.7.1"
+pkgrel = 0
+build_style = "cargo"
+hostmakedepends = [
+    "cargo-auditable",
+    "pkgconf",
+]
+makedepends = [
+    "openssl3-devel",
+    "rust-std",
+]
+pkgdesc = "Universal code formatter"
+license = "MIT"
+url = "https://topiary.tweag.io"
+source = f"https://github.com/tweag/topiary/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "3d7495caf3c0ae234bd6def6f33193e026564f7818d5909641be119de811f18e"
+# Tests require network access to download tree-sitter grammars
+options = ["!check"]
+
+
+def build(self):
+    # TODO(Harper): Package `mdbook-manmunge` and use it to build the manpages
+    # self.do("make", wrksrc="docs/manpages")
+    self.cargo.build()
+
+
+def install(self):
+    self.install_bin(
+        f"target/{self.profile().triplet}/release/topiary",
+        name="topiary",
+    )
+    self.install_license("LICENSE")
